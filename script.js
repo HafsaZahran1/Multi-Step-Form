7fbc46399363e795939619de73d643f07b7e74f2
HafsaZahran1 <73903183+HafsaZahran1@users.noreply.github.com>
2022-05-05 03:41:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var Form1 = document.getElementById("Form1");
-var Form2 = document.getElementById("Form2");
-var Form3 = document.getElementById("Form3");
+ Form2 = document.getElementById("Form2"),
+ Form3 = document.getElementById("Form3"),
 
-var Next1 = document.getElementById("Next1");
-var Next2 = document.getElementById("Next2");
+ Next1 = document.getElementById("Next1"),
+ Next2 = document.getElementById("Next2"),
 
-var Back1 = document.getElementById("Back1");
-var Back2 = document.getElementById("Back2");
+ Back1 = document.getElementById("Back1"),
+ Back2 = document.getElementById("Back2"),
 
-var progress = document.getElementById("progress");
+ progress = document.getElementById("progress");
 
 Next1.onclick = function(){
     Form1.style.left="-450px";
